43d3666f109ffc2967ed2348dffbc7c84e2c7568
Tarcisio Rodrigues <tarcisiorodrigues454@gmail.com>
2021-11-29 10:01:14 -0300

    Frontend end

--- challenge/src/components/LoginBox/index.tsx
+++ challenge/src/components/LoginBox/index.tsx
@@ -1,10 +1,19 @@
 import styles from './styles.module.scss'
 import {VscGithub} from 'react-icons/vsc'
+import { useContext, useEffect } from 'react';
+import { AuthContext } from '../../context/auth';
+
+
+
+
+
+
 export function LoginBox(){
+    const {signInUrl}=useContext(AuthContext)
     return(
         <div className={styles.loginBoxWrapper}>
             <strong>Entre e compartilhe a sua mensagem</strong>
-            <a href="" className={styles.signInWIthGithub}>
+            <a href={signInUrl} className={styles.signInWithGithub}>
                 <VscGithub size="24"/>
                 Entrar com github
             </a>
--- challenge/src/components/MessageList/index.tsx
+++ challenge/src/components/MessageList/index.tsx
@@ -1,39 +1,62 @@
 import styles from './styles.module.scss'
 import logoImg from '../../assets/logo.svg'
+import io from 'socket.io'
+import {api} from '../../services/api'
+import { useEffect, useState } from 'react'
+type Message={
+    id:string;
+    text:string;
+    user:{
+        name:string;
+        avatar_url:string;
+    }
+}
+//fazendo um fila de mensagens
+const messagesQueue:Message[]=[]
+
+const socket=io('https://localhost:4000')
+socket.on('new_message',newMessage:Message=>{
+    messagesQueue.push(newMessage)
+})
 export function MessageList(){
+    const[messages,setMessages]=useState<Message[]>([])
+    useEffect(()=>{
+        const timer=setInterval(()=>{
+            if(messagesQueue.length>0){
+                setMessages(prevState=>[
+                    messagesQueue[0],
+                    prevState[0],
+                    prevState[1]
+                ].filter(Boolean))
+                messagesQueue.shift()
+            }
+        })
+    },[])
+    useEffect(()=>{
+        api.get<Message[]>('messages/last3').then(response=>{setMessages(response.data)})
+    },[])
+    
+    
     return(
-        
+       
         <div className={styles.messageListWrapper}>
             <img src={logoImg} alt="logo" />
             <ul className={styles.messageList}>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Se você não gosta do seu destino, não o aceite. Em vez disso, tenha a coragem para transformá-lo naquilo que você quer que ele seja.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/TarcisioRodrigues.png" alt="" />
+                {messages.map(message=>{
+                    return(
+                        <li key={message.id}className={styles.message}>
+                        <p className={styles.messageContent}>{message.text}</p>
+                        <div className={styles.messageUser}>
+                            <div className={styles.userImage}>
+                                <img src={message.user.avatar_url} alt={message.user.name} />
+                            </div>
+                            <span>{message.user.name}</span>
                         </div>
-                        <span>Tarcisio Rodrigues</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Se você não gosta do seu destino, não o aceite. Em vez disso, tenha a coragem para transformá-lo naquilo que você quer que ele seja.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/TarcisioRodrigues.png" alt="" />
-                        </div>
-                        <span>Tarcisio Rodrigues</span>
-                    </div>
-                </li>
-                <li className={styles.message}>
-                    <p className={styles.messageContent}>Se você não gosta do seu destino, não o aceite. Em vez disso, tenha a coragem para transformá-lo naquilo que você quer que ele seja.</p>
-                    <div className={styles.messageUser}>
-                        <div className={styles.userImage}>
-                            <img src="https://github.com/TarcisioRodrigues.png" alt="" />
-                        </div>
-                        <span>Tarcisio Rodrigues</span>
-                    </div>
-                </li>
-                
+                    </li>
+                    
+                    )
+                })}
+           
             </ul>
 
         </div>
